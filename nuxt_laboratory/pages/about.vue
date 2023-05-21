4a4040bd87394317bc01dfda63728d1b89d38af8
Hulyo <diva.149901@davao.sti.edu.ph>
2023-05-21 12:36:31 +0800

    first commit

---
+++ nuxt_laboratory/pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+
+
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Hic exercitationem accusamus harum. 
+            Aut nisi esse commodi laudantium adipisci maiores nesciunt.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+
+</style>
---
+++ nuxt_laboratory/pages/products/[id].vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>{{ product.title }}</p>
+        <p>{{ product.price }}</p>
+        <p>{{ product.id }}</p>
+
+        <!--last touch here-->
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+    const uri = 'https://fakestoreapi.com/products' + id
+
+    const { data: product } = await useFetch(uri, { key: id })
+
+    definePageMeta({
+        layout: 'products'
+    })
+</script>
+
+<style  scoped>
+
+</style>
